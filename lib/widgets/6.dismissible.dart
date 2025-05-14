88353b19e7cee7b79743c6acd8d7000715a86e6d
Alan Varghese <alanvz2212@example.com>
2025-05-14 10:39:37 +0530

    Updated

--- lib/widgets/6.dismissible.dart
+++ lib/widgets/6.dismissible.dart
@@ -8,8 +8,47 @@ class Dismissiblee extends StatefulWidget {
 }
 
 class _DismissibleeState extends State<Dismissiblee> {
+  List<String> fruits = [
+    'Apple',
+    'Mango',
+    'Orange',
+    'Mulberry',
+    'Banana',
+    'Guvava',
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Dismissible Widget')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Dismissible Widget')),
+      body: ListView.builder(
+        itemCount: fruits.length,
+        itemBuilder: (context, index) {
+          final fruit = fruits[index];
+          return Dismissible(
+            onDismissed: (direction) {
+              if (direction == DismissDirection.startToEnd) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(fruits[index]),
+                    backgroundColor: Colors.red,
+                  ),
+                );
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(fruits[index]),
+                    backgroundColor: Colors.green,
+                  ),
+                );
+              }
+            },
+            key: Key(fruit),
+            background: Container(color: Colors.red),
+            secondaryBackground: Container(color: Colors.red),
+            child: Card(child: ListTile(title: Text(fruits[index]))),
+          );
+        },
+      ),
+    );
   }
 }
